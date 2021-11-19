71d1605fed5b2592a513360723e164f6356f808e
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2021-11-19 16:59:37 +0100

    defined three points for a curve

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+// https://javascript.info/bezier-curve
+
 function setup() {
     console.log("loaded");
     createCanvas(innerWidth, innerHeight);
+
+    const points = [];
+    points.push({x: 10+500,y: 100+300});
+    points.push({x: 60+500,y: 10+300});
+    points.push({x: 110+500,y: 100+300});
+    threePointBezier(points);
 }
 
-function draw() {
-    rect(mouseX,mouseY,100,100)
+function threePointBezier(points) {
+    points.forEach(point => {
+        fill(255,0,0);
+
+        ellipse(point.x, point.y, 7, 7);
+    });
 }
