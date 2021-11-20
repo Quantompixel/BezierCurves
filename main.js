37638f72dbb086e3cfb8a69fa2fbf78b3371c692
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2021-11-20 12:14:47 +0100

    cleanup - deleted old functions

--- main.js
+++ main.js
@@ -1,73 +1,51 @@
 // https://javascript.info/bezier-curve
 
-
 function setup() {
     console.log("loaded");
     createCanvas(innerWidth, innerHeight);
     colorMode(HSB, 360, 100, 100);
 
     let one = {
-        x: 0 + innerWidth/2,
-        y: 0 + 200
+        x: 0,
+        y: 100
     };
     let two = {
-        x: 50 + innerWidth/2,
-        y: -30 + 200
+        x: 50,
+        y: 100
     };
     let three = {
-        x: 0 + innerWidth/2,
-        y: -100 + 200
+        x: 50,
+        y: 0
     };
     let four = {
-        x: 70 + innerWidth/2,
-        y: -100 + 200
-    };
-    let five = {
-        x: 0 + innerWidth/2,
-        y: -30 + 200
+        x: 100,
+        y: 0
     };
 
-    punkte = new Array(one, two, three, four, five);
-
-    // threePointBezier(points);
-
-    // console.log(drawCurvePoint(punkte,0.5));
+    punkte = new Array(one, two, three, four);
 
     let smoothLinePoints = [];
-    for (let t = 0; t < 1; t+=0.05) {
+    for (let t = 0; t <= 1; t+=0.01) {
         smoothLinePoints.push(drawCurvePoint(punkte,t)[0]);
     }
-
 
     background(0, 0, 100)
     linesBetweenPoints(smoothLinePoints);
 
+    stroke(0);
+    strokeWeight(0.5);
     punkte.forEach(point => {
         fill(0, 100, 100);
 
-        ellipse(point.x, point.y, 7, 7);
+        ellipse(point.x, point.y, 3, 3);
     });
-}
-
-function animateBezier(points, t = 0) {
-    if (t > 1) {
-        return;
-    }
-    background(100, 0, 100)
-    drawCurvePoint(points)
-
-    setTimeout(() => {
-        requestAnimationFrame(() => {
-            animateBezier(points, t + 0.005);
-        })
-    }, 30);
 }
 
 /**
  * Draws one point in a bezier-curve.
- * @param {Array} points Array of points.
- * @param {int} t Relative number that tells where on line the points should be placed.
- * @returns 
+ * @param {Array} points Array of points
+ * @param {int} t Relative number that tells where on line the points should be placed
+ * @returns The one point that lies on the bezier curve at the position t.
  */
 function drawCurvePoint(pointArray, t) {
     const color = generateRandomHSBColor();
@@ -107,6 +85,10 @@ function drawCurvePoint(pointArray, t) {
     // added return before the recursive statement
 }
 
+/**
+ * Connects points in an array with lines
+ * @param {Array} points Array of points
+ */
 function linesBetweenPoints(points) {
     if (points.length < 2) {
         return;
@@ -117,39 +99,9 @@ function linesBetweenPoints(points) {
     linesBetweenPoints(points);
 }
 
-
-function threePointBezier(points) {
-    points.forEach(point => {
-        fill(255, 0, 0);
-
-        ellipse(point.x, point.y, 7, 7);
-    });
-
-    const diff12 = {
-        x: points[0].x - points[1].x,
-        y: points[0].y - points[1].y
-    };
-    const diff23 = {
-        x: points[1].x - points[2].x,
-        y: points[1].y - points[2].y
-    };
-    for (let i = 0; i < 1; i += 0.05) {
-        const controlPoint1 = {
-            x: points[0].x - i * diff12.x,
-            y: points[0].y - i * diff12.y
-        };
-        const controlPoint2 = {
-            x: points[1].x - i * diff23.x,
-            y: points[1].y - i * diff23.y
-        };
-        fill(Math.floor(Math.random() * 255));
-        line(controlPoint1.x, controlPoint1.y, controlPoint2.x, controlPoint2.y);
-    }
-}
-
 /**
  * Generates a random HSB-Color with 100% brightness and saturation.
- * @returns 
+ * @returns Random color
  */
 function generateRandomHSBColor() {
     return {
